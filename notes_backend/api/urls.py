b009cc267e723970e1b70c9931162db41e6d2297
kott0 <rafal44334@gmail.com>
2022-05-07 15:03:55 +0200

    urls and serializer update

--- notes_backend/api/urls.py
+++ notes_backend/api/urls.py
@@ -1,8 +1,11 @@
-from nturl2path import url2pathname
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('', views.getRoutes, name='routes'),
+    path('', views.getRoutes, name="routes"),
+    path('notes/', views.getNotes, name="notes"),
+    # path('notes/create/', views.createNote, name="create-note"),
+    #path('notes/<str:pk>/update/', views.updateNote, name="update-note"),
+    #path('notes/<str:pk>/delete/', views.deleteNote, name="delete-note"),
+    path('notes/<str:pk>/', views.getNote, name="note"),
 ]
